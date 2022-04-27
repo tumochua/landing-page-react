9acca27bf5b24dabe95edd1c82caa3fd20814831
nhan trach <tumochua14012003@gmail.com>
2022-04-27 20:21:33 +0700

    web3

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -1,13 +1,89 @@
 import React from "react";
 import "./content.scss";
-
-import img from "../../assets/13.3.png";
-
+import iconGooglePlay from "../../assets/icon-google-play.png";
+import appLayout from "../../assets/Frame-1000001920.png";
+import icon from "../../assets/Frame 709.png";
 function Content() {
   return (
     <>
       <div className="ctn">
-        <img className="img-content" src={img} />
+        <div className="ctn-ctn">
+          <div className="ctn-title">
+            <div className="ctn-left">
+              <div className="ctn-web3">The Portal to Web3</div>
+              <p>All-in-One wallet improve everything in Web3 experience</p>
+              <div className="ctn-bt">
+                <button className="ctn-btn">
+                  <img src={iconGooglePlay} />
+                  <div>
+                    <p>Get it on</p>
+                    <p className="p">Google Play </p>
+                  </div>
+                </button>
+                <button className="ctn-btn">
+                  <img src={iconGooglePlay} />
+                  <div>
+                    <p>Get it on</p>
+                    <p className="p">Google Play </p>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={appLayout} />
+          </div>
+        </div>
+        <div className="web3">
+          <div className="web3-title">
+            <h2>What the Web3 ?</h2>
+            <p>
+              Copy Web3 is the idea of incentivized decentralization of data,
+              <br /> which rewards contributors and protects their privacy and
+              <br /> data on the internet
+            </p>
+          </div>
+          <div className="web3-modal">
+            <div>
+              <div className="web3-icon">
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+              </div>
+              <div className="web3-icon">
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+                <div className="icon1">
+                  <img src={icon} />
+                  <p>waves</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Header.scss";
-
+import iconhader from "../../assets/iconder.png";
 function Header() {
   return (
     <>
-      <div className="hd1">
-        hello header
-        <h2 className="h2">hader</h2>
+      <div className="header-ctn">
+        <div className="header-icon">
+          <img alt="img" src={iconhader} className="header-icon" />
+          <h2>Rubik</h2>
+        </div>
+        <div className="header-nav">
+          <ul>
+            <li>Features</li>
+            <li>Protocol</li>
+            <li>OurTeams</li>
+            <li>Docs</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div className="header-btn">
+          <button>Launch App</button>
+        </div>
       </div>
     </>
   );
