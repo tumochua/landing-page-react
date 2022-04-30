b29fe3fc935a36a61dc8f8a99e486a6fa31ca2b2
nhan trach <tumochua14012003@gmail.com>
2022-04-30 22:33:13 +0700

    todolist class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import TodoClass from "./todoclass/TodoClass";
 import TodoHook from "./todohook/TodoHook";
+import TodoClassCode from "./TodoClassCode/TodoClassCode";
 function App() {
   return (
     <div className="App">
@@ -11,16 +12,20 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/class">About</Link>
+            <Link to="/class">class</Link>
           </li>
           <li>
-            <Link to="/hook">Contact</Link>
+            <Link to="/hook">hook</Link>
+          </li>
+          <li>
+            <Link to="/class-code">code</Link>
           </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/hook" element={<TodoHook></TodoHook>}></Route>
         <Route path="/class" element={<TodoClass></TodoClass>}></Route>
+        <Route path="/class-code" element={<TodoClassCode />}></Route>
       </Routes>
     </div>
   );
