9dcf715363de9b50acd763c4a0c0f89fb99fe83c
nhan trach <tumochua14012003@gmail.com>
2022-05-01 20:01:32 +0700

    update todo hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import TodoClass from "./todoclass/TodoClass";
 import TodoHook from "./todohook/TodoHook";
 import TodoClassCode from "./TodoClassCode/TodoClassCode";
+import TodoHookCode from "./todoHookcode/TodoHookCode";
 function App() {
   return (
     <div className="App">
@@ -18,12 +19,16 @@ function App() {
             <Link to="/hook">hook</Link>
           </li>
           <li>
+            <Link to="/hook-code">hook code</Link>
+          </li>
+          <li>
             <Link to="/class-code">code</Link>
           </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/hook" element={<TodoHook></TodoHook>}></Route>
+        <Route path="/hook-code" element={<TodoHookCode />}></Route>
         <Route path="/class" element={<TodoClass></TodoClass>}></Route>
         <Route path="/class-code" element={<TodoClassCode />}></Route>
       </Routes>
