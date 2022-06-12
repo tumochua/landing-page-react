144b0f6631678403ac55c7623f1edbab5e13a0e5
nhan trach <tumochua14012003@gmail.com>
2022-06-12 19:27:30 +0700

    detele-folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./component/header/Header";
 import Content from "./component/content/Content";
 import Foot from "./component/fooot/Foot";
+
 function App() {
   return (
     <div className="App">
